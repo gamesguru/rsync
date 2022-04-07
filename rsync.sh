188dbe538902462f41e710bf922422ac2cda356f
gamesguru <mathmuncher11@gmail.com>
2022-04-06 22:50:46 -0400

    MSG associated with log file

--- rsync.sh
+++ rsync.sh
@@ -29,11 +29,12 @@ done
 # Assign and run final command
 source .env  # NOTE: requires environment file, e.g. RSYNC_SRC_DEST="/media/shane/shane4tb/ shane4tb"
 DATE=$(date -Ins)
+read -p "Type a brief message associated with the log file: " MSG
 
 mkdir -p .rsync_logs
 
 rsync_cmd="$rsync_cmd $RSYNC_SRC_DEST"
-FRIENDLY_RSYNC_CMD="$rsync_cmd 2>&1 | tee -a .rsync_logs/${real}output$DATE.txt"
+FRIENDLY_RSYNC_CMD="$rsync_cmd 2>&1 | tee -a .rsync_logs/${real}output${DATE}_MSG:$MSG.txt"
 printf "\\n\e[1;31m%s\e[0m\\n" "$FRIENDLY_RSYNC_CMD"
-echo $FRIENDLY_RSYNC_CMD > .rsync_logs/${real}output$DATE.txt
-bash -c "$rsync_cmd" 2>&1 | tee -a .rsync_logs/${real}output$DATE.txt
+echo $FRIENDLY_RSYNC_CMD > .rsync_logs/${real}output${DATE}_MSG:$MSG.txt
+bash -c "$rsync_cmd" 2>&1 | tee -a .rsync_logs/${real}output${DATE}_MSG:$MSG.txt
